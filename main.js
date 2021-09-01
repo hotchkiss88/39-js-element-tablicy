b35ae70598e11f6f5e75b4e1c0039571d7b8c7db
hotchkiss88 <al-sowa@hotmail.com>
2021-09-01 13:22:32 +0200

    operator porównaia logiczy

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let tablica = [100, 1, 2, 3, 4, 5, 6];
 
-function first(tablica) {
-    if (Array.isArray(tablica)) return;
-    return tablica[0];
+function first(arr) {
+    if (!Array.isArray(arr)) return;
+    return arr[0];
 }
 
-console.log(tablica[0]);
+console.log(first(tablica));
